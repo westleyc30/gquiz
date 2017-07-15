849879d30aac4410296524de7a6a85f224e2db99
Westley Cho <westleyc30@users.noreply.github.com>
2017-07-14 17:33:42 -0700

    basic button functionality

---
+++ main.js
@@ -0,0 +1,26 @@
+const chordButton = document.querySelectorAll('.button__chord');
+// console.log(chordButton)
+let chordList = {};
+
+const requestURL = 'https://raw.githubusercontent.com/westleyc30/gquiz/master/chords.json';
+const request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = () => {
+  chordList = request.response;
+  // console.log(chordList.chords.a.major.audio)
+}
+
+// This function creates an array from chordButton querySelectorAll
+// for each button in that array we add an event listener to play the chord audio
+Array.from(chordButton).forEach(button => {
+  button.addEventListener('click', function(e) {
+    let currentChord = this.dataset.chord;
+    // console.log(this);
+    console.log(chordList);
+    console.log(`guitar.${currentChord}.audio`);
+    console.log(currentChord);
+  })
+})
